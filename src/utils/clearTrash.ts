79aa0a3b50f494678abe845e53d79374b44f7b24
mcbalaam <mr.max.kh@gmail.com>
2025-06-26 00:41:30 +0700

    clear trash fix

--- src/utils/clearTrash.ts
+++ src/utils/clearTrash.ts
@@ -5,29 +5,29 @@ const LOGPATH = path.join(__dirname, "..", "..", "logs");
 const STORAGEPATH = path.join(__dirname, "..", "..", "storage");
 
 if (fs.existsSync(LOGPATH)) {
-	const files = fs.readdirSync(LOGPATH);
-	
-	files.forEach(file => {
-		const filePath = path.join(LOGPATH, file);
-		
-		if (fs.statSync(filePath).isFile()) {
-			fs.unlinkSync(filePath);
-		}
-	});
-	
-	console.log("Log history cleared");
+  const files = fs.readdirSync(LOGPATH);
+
+  files.forEach((file) => {
+    const filePath = path.join(LOGPATH, file);
+
+    if (fs.statSync(filePath).isFile()) {
+      fs.unlinkSync(filePath);
+    }
+  });
+
+  console.log("Log history cleared");
 }
 
 if (fs.existsSync(STORAGEPATH)) {
-	const files = fs.readdirSync(STORAGEPATH);
-	
-	files.forEach(file => {
-		const filePath = path.join(STORAGEPATH, file);
-		
-		if (fs.statSync(filePath).isFile()) {
-			fs.unlinkSync(filePath);
-		}
-	});
-	
-	console.log("Release files deleted");
+  const files = fs.readdirSync(STORAGEPATH);
+
+  files.forEach((file) => {
+    const filePath = path.join(STORAGEPATH, file);
+
+    if (fs.statSync(filePath).isDirectory()) {
+      fs.rmSync(filePath, { recursive: true, force: true });
+    } else {
+      fs.unlinkSync(filePath);
+    }
+  });
 }
